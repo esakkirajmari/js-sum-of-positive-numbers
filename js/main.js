bedd36c6ea60ef7202441f781c0f6fc9912d292c
esakkirajmari <esakkirajmari@gmail.com>
2022-12-14 22:50:15 +0530

    js/

--- js/main.js
+++ js/main.js
@@ -1,10 +1,9 @@
 let num=parseInt(prompt("enter the a value"));
 let sum=0;
 for (i=1;i<=num;i++){
-   let  b=parseInt(prompt("enter the b value"));
-   
+     b=parseInt(prompt("enter the b value"));
     if(b>0){
         sum+=b;
     }
 }
-document.write(sum);
+    document.write(sum);
